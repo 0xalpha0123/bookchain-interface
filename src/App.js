028c0f0dfeb240386913924bc8af844ae13dd390
wayne-th <wayned.eth@gmail.com>
2017-03-30 14:51:08 -0600

    adds basic form, and styling to root page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
+import BookForm from './BookForm.js'
 
 const ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 const bookContractABI = [{"constant":false,"inputs":[],"name":"checkout","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"status","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"returnBook","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"borrower","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isAvailable","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"author","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"inputs":[{"name":"_name","type":"bytes32"},{"name":"_author","type":"bytes32"}],"payable":false,"type":"constructor"}]
@@ -13,14 +14,15 @@ class App extends Component {
     super(props)
     this.state = {
       isAvailable: [],
-      owner: []
+      owner: [],
     }
   }
   componentWillMount() {
     let data = bookContract
     this.setState({
       isAvailable: String(data.isAvailable()),
-      owner: String(data.owner())
+      owner: String(data.owner()),
+
     })
   }
   render() {
@@ -28,7 +30,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>BookCHAIN!!!!111!!</h2>
+          <h2>Bookchain</h2>
         </div>
         <div className="App-intro">
           <section>
@@ -39,6 +41,9 @@ class App extends Component {
           <section>
             ...AND this should be a crazy blockchain address for the contract owner.. { this.state.owner }
           </section>
+          <BookForm name='name' />
+          <BookForm name='author' />
+          <BookForm name='isdn' />
         </div>
       </div>
     );
