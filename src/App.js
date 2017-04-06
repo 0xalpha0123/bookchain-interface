39a2c0ff0bfe3b999ddca463ea1b5889946da889
wayne-th <wayned.eth@gmail.com>
2017-04-06 15:36:44 -0600

    We need to figure out why the this.setState isn't being set ln:72

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BookForm from './BookForm.js';
 import Carousel from './Carousel';
 import bookContract from './ethereum/EthereumClient';
-
+const request = require('superagent');
 const bookContractAddress = '0x468a2507dd1d438c42160390d3d7a8d47bec8765';
 
 class App extends Component {
@@ -13,28 +15,28 @@ class App extends Component {
     this.state = {
       isAvailable: [],
       owner: [],
-      books: [
-        {
+      testBook: [],
+      books: [{
           title: "Don Quixote",
           author: "Miguel de Cervantes",
           isdn: "1"
         },
-         {
+        {
           title: "Ulysses",
           author: "James Joyce",
           isdn: "2"
         },
-         {
+        {
           title: "The Odyssey",
           author: "Homer",
           isdn: "3"
         },
-         {
+        {
           title: "Don Quixote",
           author: "Miguel de Cervantes",
           isdn: "4"
         },
-         {
+        {
           title: "Moby Dick",
           author: "Herman Melville",
           isdn: "5"
@@ -45,55 +47,32 @@ class App extends Component {
 
   componentWillMount() {
     let data = bookContract;
-    let bookIsbn = "0316067598";
-    const url = "https://www.googleapis.com/books/v1/volumes?q=isbn" +
-      bookIsbn;
 
-    fetch(url)
-      .then(function(response) {
-        if (response.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return response.json();
-      })
-      .then(function(data) {
-        this.setState({
-          book: data.items[0]
-        });
-      }).done();
     this.setState({
       isAvailable: String(data.isAvailable()),
       owner: String(data.owner())
     });
   }
-  // componentWillMount() {
-  //   fetch("http://date.jsontest.com/")
-  //     .then((response) => response.json())
-  //     .then((responseData) => {
-  //       console.log('a');
-  //       this.setState({date: responseData.date});
-  //     })
-  //     .done();
-  // }
+
+  componentDidMount() {
+    var x = this.getBookData();
+    debugger;
+  }
 
   getBookData() {
     let bookIsbn = "0316067598";
     const url = "https://www.googleapis.com/books/v1/volumes?q=isbn" +
       bookIsbn;
+    var currentThis = this;
 
-    fetch(url)
-      .then(function(response) {
-        if (response.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return response.json();
-      })
-      .then(function(data) {
-        this.setState({
-          book: data.items[0]
-        });
-        var bookData = data.items[0];
-        debugger;
+    request
+      .get(url)
+      .end((err, res) => {
+        var bookData = res.body.items[0].volumeInfo;
+        // return currentThis.setState({
+          // testBook: bookData
+        // });
+        return bookData;
       });
   }
 
@@ -116,6 +95,8 @@ class App extends Component {
           <br/>
           <section>
             ...AND this should be a crazy blockchain address for the contract owner.. { this.state.owner }
+            { this.state.testBook }
+          <p>{this.state.testBook.title}</p>
           </section>
           <BookForm name='name' />
           <BookForm name='author' />
